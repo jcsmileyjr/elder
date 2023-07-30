7f53cec37a0470259ee224ff2ae00b78487c1d8e
Smiley <jcsmileyjr@gmail.com>
2023-07-30 15:24:49 -0500

    Feature: Initial log off button as a footer

---
+++ src/app/footer/page.js
@@ -0,0 +1,17 @@
+import styles from './footer.module.css';
+import { useRouter } from 'next/navigation';
+
+const Footer = () => {
+    const router = useRouter() // Routes a user to another page
+    const logUserOff = () => {
+        
+        router.push("/");
+    }
+    return (
+        <footer className={styles.footer}>
+            <button type="button" className={`${styles.entryButton} ${styles.logOff}`} onClick={() => logUserOff()}> Log Off</button>
+        </footer>
+    )
+}
+
+export default Footer;
