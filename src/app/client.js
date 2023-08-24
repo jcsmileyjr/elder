7e767b9aa422baf045eb91e2f3ab582f3771ace6
Smiley <jcsmileyjr@gmail.com>
2023-08-24 09:23:11 -0500

    Refactor Sanity client to use up to date function

--- src/app/client.js
+++ src/app/client.js
@@ -1,8 +1,9 @@
-import sanityClient  from "@sanity/client";
+import {createClient}  from "@sanity/client";
 
-const client = sanityClient({
+const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_API_DATASET,
+    apiVersion: '2023-05-03',
     useCdn: true // `false` if you want to ensure fresh data
 })
 
