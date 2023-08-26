71130e4624a71e0939df402c7b3483bbb4291212
Smiley <jcsmileyjr@gmail.com>
2023-08-26 16:58:15 -0500

    Bug: ProjectID key isn't accepted in Prod

--- src/app/libs/api/client.js
+++ src/app/libs/api/client.js
@@ -1,9 +1,20 @@
 import {createClient}  from "@sanity/client";
 
+let envProjectId, envDataset, envToken;
+if (process.env.NODE_ENV === 'development') {
+    envProjectId = process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID;
+    envDataset = process.env.NEXT_PUBLIC_SANITY_API_DATASET;
+    envToken = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN;
+}else {
+    envProjectId = SANITY_API_PROJECT_ID;
+    envDataset = SANITY_API_DATASET;
+    token = SANITY_API_WRITE_TOKEN;
+}
+
 const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_API_DATASET,
-    token: process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN,
+    projectId: envProjectId,
+    dataset: envDataset,
+    token: envToken,
     apiVersion: '2023-05-03',
     useCdn: true // `false` if you want to ensure fresh data
 })
