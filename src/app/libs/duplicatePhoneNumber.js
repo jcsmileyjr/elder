2ab93998aef6d281642e5c0f6526c3af84fd5619
Smiley <jcsmileyjr@gmail.com>
2023-08-29 17:07:22 -0500

    Bug fix: Trying to force a backend update

--- src/app/libs/duplicatePhoneNumber.js
+++ src/app/libs/duplicatePhoneNumber.js
@@ -2,24 +2,24 @@ import getAllElder from "./api/getElder";
 
 /**
  * Takes a 10 digit phone number and compare array of elder objects phone number property to ensure its not a duplicate.
- * @param {string} phoneNumber 
+ * @param {string} phoneNumber
  * @returns true or false
  */
 const ifDuplicatePhoneNumber = async (phoneNumber) => {
+  console.log("Trying to force an backend update");
   let parseData = await getAllElder();
 
   // Check if the phone number is already being used by an object in the array of elders
   let foundIndex = parseData.findIndex((data) => {
-      return data.phoneNumber === phoneNumber;
+    return data.phoneNumber === phoneNumber;
   });
 
   // If the number isn't found, return false. If found, return true
-  if(foundIndex === -1) {
-      return false;
+  if (foundIndex === -1) {
+    return false;
   } else {
-      return true;
+    return true;
   }
-}
+};
 
-export { ifDuplicatePhoneNumber }
-
+export { ifDuplicatePhoneNumber };
