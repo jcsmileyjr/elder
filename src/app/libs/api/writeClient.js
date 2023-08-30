13cd395f7d15b770db0f6e6f0fdac2a4b4b3643e
Smiley <jcsmileyjr@gmail.com>
2023-08-30 17:03:15 -0500

    Documentation: Removing debugging code

--- src/app/libs/api/writeClient.js
+++ src/app/libs/api/writeClient.js
@@ -1,22 +1,9 @@
 import { createClient } from "@sanity/client";
 
 let permissions = process.env.SANITY_API_WRITE_TOKEN;
-console.log("permissions: ", process.env.SANITY_API_WRITE_TOKEN);
+
 if (process.env.NODE_ENV === "development") {
     permissions = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN;
-    console.log("DEV permissions: ", process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN);
-} else {
-  console.log("PROD permissions");
-  console.error("prod permission",process.env.SANITY_API_WRITE_TOKEN)
-}
-
-const apiKey = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN
-if(!apiKey) {
-  console.log("API Key not found in environment variables");
-  console.error("API Key not found in environment variables")
-} else {
-  console.log("Length of the API key is: " + apiKey?.length);
-  console.error("Length of the API key is: " + apiKey?.length);
 }
 
 const writeClient = createClient({
