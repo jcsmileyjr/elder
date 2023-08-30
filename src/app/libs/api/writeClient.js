dda800b6f8fea682766b5378f0772134a13ff2d4
Smiley <jcsmileyjr@gmail.com>
2023-08-29 19:16:59 -0500

    Bug: Checking if the permission is correct

--- src/app/libs/api/writeClient.js
+++ src/app/libs/api/writeClient.js
@@ -1,19 +1,20 @@
-import {createClient}  from "@sanity/client";
+import { createClient } from "@sanity/client";
 
 let permissions = process.env.SANITY_API_WRITE_TOKEN;
-if(process.env.NODE_ENV === 'development') {
-    console.log("DEV permissions")
-    permissions = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN;
-}else {
-    console.log("PROD permissions")
+console.log("permissions: ", process.env.SANITY_API_WRITE_TOKEN);
+if (process.env.NODE_ENV === "development") {
+  console.log("DEV permissions");
+  permissions = process.env.NEXT_PUBLIC_SANITY_API_WRITE_TOKEN;
+} else {
+  console.log("PROD permissions");
 }
 
 const writeClient = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    token: permissions,
-    apiVersion: '2023-05-03',
-    useCdn: false // `false` if you want to ensure fresh data
-})
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  token: permissions,
+  apiVersion: "2023-05-03",
+  useCdn: false, // `false` if you want to ensure fresh data
+});
 
 export default writeClient;
