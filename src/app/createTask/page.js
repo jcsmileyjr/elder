a25e97f956a8a652ef0afa7456991c049c261340
Smiley <jcsmileyjr@gmail.com>
2023-09-22 08:02:00 -0500

    Documentation
    Remove console logs, updated comments, and fine tune the UI

--- src/app/createTask/page.js
+++ src/app/createTask/page.js
@@ -8,38 +8,28 @@ import PlusSign from '../plus-sign.png';
 import Image from 'next/image';
 
 /**
- * TODO
- * Get data for name in the Header component - done
- * Setup useState/useEffect to get current essential tasks - done
- * Setup the UI to add an essential task - done
- * Copy and tweak from accountCreation ability to add a Daily, weekly, monthly essentail tasks with content. - done
- * Find a way to update the database with the old and new content
- * 
- * Documentation: remove unused information, console.logs, add comments, etc.
+ * App functionality to update an account's essential tasks
  * @returns 
  */
 const CreateTask = () => {
     const router = useRouter() //  Use to relocate user to another page
 
     const [entries, setEntries] = useState([]) // Get the current loved one data
-    const [essentialTasks, setessentialTasks] = useState([]) // Current essential tasks for the love one
-    const [dailyTasks, setDailyTasks] = useState([]);
-    const [currentDailyTasks, setCurrentDailyTasks] = useState("");
-    const [weeklyTasks, setWeeklyTasks] = useState([]);
-    const [currentWeeklyTasks, setCurrentWeeklyTasks] = useState("");
-    const [monthlyTasks, setMonthlyTasks] = useState([]);
-    const [currentMonthlyTasks, setCurrentMonthlyTasks] = useState("");
-    const [oldDailyTasks, setOldDailyTasks] = useState([])
-    const [oldWeeklyTasks, setOldWeeklyTasks] = useState([])
-    const [oldMonthlyTasks, setOldMonthlyTasks] = useState([])
+    const [dailyTasks, setDailyTasks] = useState([]); // Array of daily tasks
+    const [currentDailyTasks, setCurrentDailyTasks] = useState(""); // UI content enter by the author for daily tasks
+    const [weeklyTasks, setWeeklyTasks] = useState([]); // Array of weekly tasks
+    const [currentWeeklyTasks, setCurrentWeeklyTasks] = useState(""); // UI content enter by the author for weekly tasks
+    const [monthlyTasks, setMonthlyTasks] = useState([]); // Array of monthly tasks
+    const [currentMonthlyTasks, setCurrentMonthlyTasks] = useState(""); // UI content enter by the author for monthly tasks
     
     useEffect(() => {
+        // Get the current elder data
         let previousSavedData = localStorage.getItem("Elder-data");
         const parseData = JSON.parse(previousSavedData)
         setEntries(parseData); // Need this to get the name of the care one
         
+        // Loads the current elder's previous tasks to be viewable by the author
         let previousDailyTasks = parseData.essentialTasks.find(grouping => grouping.type === "Daily")
-        //setDailyTasks([...dailyTasks, previousDailyTasks.tasks]);
         setDailyTasks(previousDailyTasks.tasks);
 
         let previousWeeklyTasks = parseData.essentialTasks.find(grouping => grouping.type === "Weekly")
@@ -49,58 +39,37 @@ const CreateTask = () => {
         setMonthlyTasks(previousMonthlyTasks.tasks);
     }, [])
 
-    // Disable the Done button if there isn't 10 numbers for the phone number and at least one for name
-    const enabledDoneButton = () => {
-        //console.log("works")
-    }
-
-    //Add text to a array of Essential daily tasks
+    //Add a new task to a array of Essential daily tasks
     const addDailyTask = () => {
         if(currentDailyTasks !== "") {
             setDailyTasks([...dailyTasks, currentDailyTasks]);
             setCurrentDailyTasks("");
-            console.log(dailyTasks);
         }
     }
 
-        //Add text to a array of Essential weekly tasks
+    //Add a new task to a array of Essential weekly tasks
     const addWeeklyTask = () => {
         if(currentWeeklyTasks !== "") {
             setWeeklyTasks([...weeklyTasks, currentWeeklyTasks]);
             setCurrentWeeklyTasks("");
-            console.log(weeklyTasks);
         }
     }
 
-        //Add text to a array of Essential monthly tasks
+    //Add a new task to a array of Essential monthly tasks
     const addMonthlyTask = () => {
         if(currentMonthlyTasks !== "") {
             setMonthlyTasks([...monthlyTasks, currentMonthlyTasks]);
             setCurrentMonthlyTasks("");
-            console.log(monthlyTasks);
         }
     }
 
-    /**
-     * TODO
-     * Get the current elder
-     * Create an array of tasks
-     * Update the elder essenetial tasks
-     * Save the elder
-     * Move back to viewEntries page
-     * 
-     * Assign the elder a new array of tasks objects
-     */
+    // Assign the elder a new array of tasks objects
     const updateTasks = () => {
         // Get the current elder info
         let previousSavedData = localStorage.getItem("Elder-data");
         let parseSavedData = JSON.parse(previousSavedData);
 
-        // let newEssentialTasksArray = [];
-        // newEssentialTasksArray.push(dailyTasks);
-        // newEssentialTasksArray.push(weeklyTasks);
-        // newEssentialTasksArray.push(monthlyTasks);
-
+        // Create a new array of essential tasks objects and add to the current elder data
         let newEssentialTasksArray = [
             {
                 "type":"Daily",
@@ -121,12 +90,10 @@ const CreateTask = () => {
 
         parseSavedData.essentialTasks = newEssentialTasksArray;
 
-        console.log(newEssentialTasksArray);
-
-        // Update the current elder information
+        // Update the current elder information within local storeage for direct app useage
         localStorage.setItem("Elder-data", JSON.stringify(parseSavedData));
 
-        // Update the pretend database (Elder-test-data) with the now updated current elder
+        // Update the database with the now updated current elder
         updateDatabase(parseSavedData);
         router.push('/viewEntries')
     }
@@ -153,7 +120,7 @@ const CreateTask = () => {
                 <ul>
                     {dailyTasks.length > 0 &&                            
                         dailyTasks.map( (tasks, index) => (
-                            <li className={styles.taskStyles} key={index + 'dailytask'}>Task: {tasks}</li>
+                            <li className={styles.taskStyles} key={index + 'dailytask'}><span className={styles.label}>Task:</span> {tasks}</li>
                         ))
                     }
                 </ul>
@@ -170,7 +137,7 @@ const CreateTask = () => {
                 <ul>
                     {weeklyTasks.length > 0 &&                            
                         weeklyTasks.map( (tasks, index) => (
-                            <li className={styles.taskStyles} key={index + 'weeklytask'}>{tasks}</li>
+                            <li className={styles.taskStyles} key={index + 'weeklytask'}><span className={styles.label}>Task:</span> {tasks}</li>
                         ))
                     }
                 </ul>
@@ -187,13 +154,15 @@ const CreateTask = () => {
                 <ul>
                     {monthlyTasks.length > 0 &&                            
                         monthlyTasks.map( (tasks, index) => (
-                            <li className={styles.taskStyles} key={index + 'monthlytask'}>{tasks}</li>
+                            <li className={styles.taskStyles} key={index + 'monthlytask'}><span className={styles.label}>Task:</span> {tasks}</li>
                         ))
                     }
                 </ul>
             </div>
+
+            {/* Continue Button */}
             <div className={styles.doneButtonContainer}>
-                <button onClick={() => updateTasks()} disabled={enabledDoneButton()} className={`${styles.entryButton} ${styles.doneButtonStyle}`} type='button'>Continue</button>
+                <button onClick={() => updateTasks()} className={`${styles.entryButton} ${styles.doneButtonStyle}`} type='button'>Done</button>
             </div>                    
                 
             </main>
